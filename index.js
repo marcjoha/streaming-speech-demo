f944a00a3f7b6475ee65b650cf37e87cc7f7571d
Marcus Johansson <majohansson@gmail.com>
2019-01-17 23:33:47 +0100

    fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var micStream;
 // Wait until start button is clicked
 document.getElementById('start-button').onclick = () => {
   // Wire up streamable socket and mic
-  socket = io({ transports: ['websocket'] });
+  socket = io();
   socketStream = ss.createStream({ objectMode: true });
   micStream = new MicrophoneStream({ objectMode: true });
 
